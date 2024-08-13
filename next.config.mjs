689d485be31e5def4ed3a15dd575b7cee3360abc
a1740942002 <a1740942002@me.com>
2024-08-13 15:34:41 +0800

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
+import removeImports from 'next-remove-imports'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
+  reactStrictMode: true
+}
 
-export default nextConfig;
+/** @type {function(import("next").NextConfig): import("next").NextConfig}} */
+const removeImportsFun = removeImports({
+  test: /node_modules([\s\S]*?)\.(tsx|ts|js|mjs|jsx)$/,
+  matchImports: '\\.(less|css|scss|sass|styl)$'
+})
+
+export default removeImportsFun({
+  ...nextConfig,
+  webpack(config, options) {
+    return config
+  }
+})
